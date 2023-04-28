9e19836949220804688e6f79172417d5390af81a
Luciano Vega <bubu05.lv@gmail.com>
2023-04-28 17:32:09 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Enumerados.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal enum Protagonistas
+    {
+        Kayle,
+        MetalSlug,
+        Sasuke,
+    }
+
+    internal enum Habilidades
+    { 
+        Curar,
+        GolpeDoble,
+        SerInvisible,
+    }
+}
---
+++ Entidades/Personajes.cs
@@ -0,0 +1,40 @@
+﻿namespace Entidades
+{
+    public abstract class Personaje
+    {
+        protected int cantidadDeVida;
+        protected string nombre;
+        protected int dañoBase;
+        protected int nivel;
+
+
+        public Personaje(int cantidadDeVida, string nombre, int curacion, int nivel)
+        {
+            this.cantidadDeVida = cantidadDeVida;
+            this.nombre = nombre;
+            this.nivel = nivel;
+        }
+
+        public int CantidadDeVida { get => cantidadDeVida; set => cantidadDeVida = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Nivel { get => nivel; set => nivel = value; }
+        public int DañoBase { get => dañoBase; set => dañoBase = value; }
+
+        internal abstract int Atacar() 
+        {
+            
+        }
+
+        internal bool Morir() 
+        {
+            bool retorno =  false;
+
+            if (this.cantidadDeVida == 0) 
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+    }
+}
